052d48db7c1890f54a921933e83384b85d8b1f71
Ezaz khan <90470272+ezazkhan0786@users.noreply.github.com>
2021-12-26 12:25:58 +0530

    Create String.py

---
+++ String.py
@@ -0,0 +1,4 @@
+age = 25
+print("My name is %s. And my friend's name is %s. My age is %i" % (name, name2, age))
+print(f"My name is {name}. And my friend's name is {name2}.My age is {age}")
+print(a,"Pritam", "Kumar")
