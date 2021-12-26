bb2b53d6fe5c54fc33c0e4e0fb1725aff2e142c7
Ezaz khan <90470272+ezazkhan0786@users.noreply.github.com>
2021-12-26 12:24:50 +0530

    Create input.py

---
+++ input.py
@@ -0,0 +1,15 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+salary = float(input("Enter your salary: "))
+unemployed = bool(input("Are you unemployed? "))
+
+print(name)
+print(age)
+print(salary)
+print(unemployed)
+
+s1 = "1"
+s2 = ""
+
+print(bool(s1))
+print(bool(s2))
